0ef2ae10b64afb2610c332616b568c8f36347355
Wamin Suwito <waminsuwito66@gmail.com>
2025-09-09 01:49:46 +0000

    gunakan settingan ini // For Firebase JS SDK v7.20.0 and later, measurem

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,15 +2,17 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore, collection, getDocs, getDoc, setDoc, doc, addDoc, updateDoc, deleteDoc, serverTimestamp, onSnapshot, Timestamp, query, where, limit, orderBy, runTransaction, FieldValue, increment, writeBatch } from 'firebase/firestore';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyD6F-pfLuauK6nQyB6-1MZl56-J6B0PTnc",
+  authDomain: "laporan-b9456.firebaseapp.com",
+  projectId: "laporan-b9456",
+  storageBucket: "laporan-b9456.appspot.com",
+  messagingSenderId: "705126693863",
+  appId: "1:705126693863:web:a234a7d7d2022b76491b42",
+  measurementId: "G-CMFF728Y20"
 };
+
 
 // Inisialisasi Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
